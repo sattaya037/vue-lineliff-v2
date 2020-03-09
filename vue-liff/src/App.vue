b0bc14796f6a08c4dcc704e9d5db2eaa11ba693c
sattaya037 <sattaya.ra@ics-th.com>
2020-03-09 16:12:00 +0700

    make it better

--- vue-liff/src/App.vue
+++ vue-liff/src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  beforeCreate(){
+        this.$liff.init({
+           liffId: "1653924762-Wmvo8BAG"
+        })
+        .then(() => {
+          if(!this.$liff.isLoggedIn()){
+              this.$liff.login(); 
+              console.log("liff")
+          }else{
+            console.log("not login")
+          }
+            // start to use LIFF's api
+            
+        })
   }
+
+
+
+
+  // name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
 }
+
 </script>
 
 <style>
--- vue-liff/src/main.js
+++ vue-liff/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.prototype.$liff = window.liff
 Vue.config.productionTip = false
 
 new Vue({
