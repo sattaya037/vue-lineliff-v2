776677c7341e9a003d285dac4ea113456b4772b8
sattaya037 <sattaya.ra@ics-th.com>
2020-03-10 10:55:59 +0700

    make it better

--- vue-liff/src/App.vue
+++ vue-liff/src/App.vue
@@ -5,6 +5,9 @@
     <HelloWorld msg="Welcome to Your Vue.js App"/>
         <h3>Home</h3>
         <h3>{{displayName}}</h3>
+        <v-avatar tile color="blue">
+           <v-icon dark>mdi-alarm</v-icon>
+        </v-avatar>
         <h5>{{statusMessage}}</h5>
 
     <nav>
