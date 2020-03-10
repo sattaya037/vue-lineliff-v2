38035ffdd41625f7ebfbaa0d3eab72ada921cb15
sattaya037 <sattaya.ra@ics-th.com>
2020-03-10 10:06:13 +0700

    make it better

--- vue-liff/src/App.vue
+++ vue-liff/src/App.vue
@@ -7,7 +7,7 @@
         <h3>{{displayName}}</h3>
 
     <nav>
-        <router-link to='/'>Home</router-link>
+        <!-- <router-link to='/'>Home</router-link> -->
         <router-link to='/register'>Register</router-link>
         <router-link to='/login'>Login</router-link>
     </nav>
